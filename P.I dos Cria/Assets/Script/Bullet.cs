5b7925e22ec1058f1363069335fd2201e4464f85
FranSch2000 <167346510+FranSch2000@users.noreply.github.com>
2025-04-14 14:00:33 -0300

    Mecanicas Player
    Mecânicas de tiro, andar vertical e horizontal e enemy estão feitas. vou fazer um script de vida para o player depois.

---
+++ P.I dos Cria/Assets/Script/Bullet.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 10f;
+    public int damage = 1;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.linearVelocity = transform.up * speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            EnemyHealth enemy = collision.gameObject.GetComponent<EnemyHealth>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+        }
+
+        Destroy(gameObject); 
+    }
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
+
