6ee1f655d23f65b7da36b3d97fe9fccd16a9ce4d
FranSch2000 <167346510+FranSch2000@users.noreply.github.com>
2025-04-23 14:20:27 -0300

    ADD

---
+++ P.I dos Cria/Assets/Script/EnemyMov.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class EnemyMov : MonoBehaviour
+{
+    private Transform playerPosition;
+    public float speedEnemy = 3f;
+    public float followRange = 5f; 
+
+    void Start()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            playerPosition = player.transform;
+        }
+    }
+
+    void Update()
+    {
+        if (playerPosition != null)
+        {
+            float distance = Vector2.Distance(transform.position, playerPosition.position);
+
+
+            if (distance <= followRange)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, playerPosition.position, speedEnemy * Time.deltaTime);
+            }
+        }
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, followRange);
+    }
+}
