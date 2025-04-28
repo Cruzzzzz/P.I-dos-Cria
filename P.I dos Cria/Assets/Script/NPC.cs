cd80f86dd06183c20fe1f1f2d33305cdbc8100b6
FranSch2000 <167346510+FranSch2000@users.noreply.github.com>
2025-04-28 15:59:04 -0300

    NPC
    Mecânica de NPC adicionada para o jogo. Por enquanto o NPC só tem a mecânica de fala! Futuramente tera a mecanica de ataque.

---
+++ P.I dos Cria/Assets/Script/NPC.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class NPC : MonoBehaviour, Iinteractable
+{
+    public NPCdialogue dialogueData;
+    public GameObject dialoguePanel;
+    public TMP_Text dialogueText, nameText;
+    public Image portraitImage;
+
+    private int dialogueIndex;
+    private bool isTyping, isDialogueActive;
+
+     public void Interact()
+    {
+        if(dialogueData == null)
+        {
+            return;
+        }
+        if(isDialogueActive ) {
+    }
+
+    public bool CanInteract()
+    {
+        return !isDialogueActive;
+    }
+}
---
+++ P.I dos Cria/Assets/Script/NPC_Dialogue.cs
@@ -0,0 +1,91 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections;
+
+public class NPC_Dialogue : MonoBehaviour
+{
+    public string[] dialogueNPC;
+    public int dialogueIndex;
+
+    public GameObject dialoguePanel;
+    public TMP_Text dialogueText;
+
+    public TMP_Text nameNPC;
+    public Image imageNPC;
+    public Sprite spriteNPC;
+
+    public bool readyToSpeak;
+    public bool startDialogue;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        dialoguePanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && readyToSpeak)
+        {
+            if (!startDialogue)
+            {
+                FindAnyObjectByType<Player>().speed = 0f;
+                StartDialogue();
+            }
+            else if (dialogueText.text == dialogueNPC[dialogueIndex])
+            {
+                NextDialogue();
+            }
+
+        }
+    }
+    void NextDialogue()
+    {
+        dialogueIndex++;
+        if (dialogueIndex < dialogueNPC.Length)
+        {
+            StartCoroutine(ShowDialogue());
+        }
+        else
+        {
+            dialoguePanel.SetActive(true);
+            startDialogue = false;
+            dialogueIndex = 0;
+            FindAnyObjectByType<Player>().speed = 5f;
+        }
+    }
+    void StartDialogue()
+    {
+        nameNPC.text = "Marcos";
+        imageNPC.sprite = spriteNPC;
+        startDialogue = true;
+        dialogueIndex = 0;
+        dialoguePanel.SetActive(true);
+        StartCoroutine(ShowDialogue());
+    }
+    IEnumerator ShowDialogue()
+    {
+        dialogueText.text = "";
+        foreach (char letter in dialogueNPC[dialogueIndex])
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(0.1f);
+        }   
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            readyToSpeak = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            readyToSpeak = false;
+        }
+    }
+}
