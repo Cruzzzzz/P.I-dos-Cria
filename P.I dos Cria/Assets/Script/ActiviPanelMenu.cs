a16bde8a025c1f78954e2577fe8afab128ecb338
FranSch2000 <167346510+FranSch2000@users.noreply.github.com>
2025-05-10 11:08:10 -0300

    Criação de Mapa
    Desenvolvimento do mapa/game em andamento.

--- P.I dos Cria/Assets/Script/ActiviPanelMenu.cs
+++ P.I dos Cria/Assets/Script/ActiviPanelMenu.cs
@@ -3,41 +3,32 @@ using UnityEngine;
 
 public class ActiviPanelMenu : MonoBehaviour
 {
-    [SerializeField] private GameObject panel; // Referência ao Panel no Inspector
+    [SerializeField] private GameObject panel;
     private bool isPaused = false;
 
     void Update()
     {
-        // Verifica se a tecla ESC foi pressionada
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            // Inverte o estado de ativação do Panel e pausa/despausa o jogo
             TogglePanelAndPause();
         }
     }
-
-    // Método para alternar o estado do Panel e pausa do jogo
     public void TogglePanelAndPause()
     {
         if (panel != null)
         {
-            // Inverte o estado atual
             isPaused = !isPaused;
-
-            // Ativa/desativa o panel
             panel.SetActive(isPaused);
-
-            // Pausa/despausa o jogo
             if (isPaused)
             {
-                Time.timeScale = 0f; // Pausa o jogo
-                AudioListener.pause = true; // Pausa os áudios
+                Time.timeScale = 0f; 
+                AudioListener.pause = true; 
                 
             }
             else
             {
-                Time.timeScale = 1f; // Despausa o jogo
-                AudioListener.pause = false; // Despausa os áudios
+                Time.timeScale = 1f; 
+                AudioListener.pause = false; 
             }
         }
         else
@@ -45,9 +36,7 @@ public class ActiviPanelMenu : MonoBehaviour
             Debug.LogWarning("Nenhum Panel atribuído ao script TogglePanelWithESC");
         }
     }
-
-    // Método opcional para fechar o panel e despausar por botão UI
-    public void ClosePanelAndUnpause()
+    public void ClosePanelAndUnpause() //Opicional!
     {
         isPaused = false;
         panel.SetActive(false);
