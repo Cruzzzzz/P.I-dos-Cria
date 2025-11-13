38ade1588d8862f12e174c7e1cc746086634e4a7
FranSch2000 <167346510+FranSch2000@users.noreply.github.com>
2025-11-13 09:12:16 -0300

    Mudanças na Gameplay
    Agora podemos fazer Enemys diferenciados como: Enemy rápidos, fortes, e tanques(Mais vida).

--- P.I dos Cria/Assets/Script/EnemyHealth.cs
+++ P.I dos Cria/Assets/Script/EnemyHealth.cs
@@ -1,30 +1,36 @@
-using System.Collections.Generic;
-using NUnit.Framework;
 using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
-    public float maxHealth = 3;
+    [Header("Configurações de Vida")]
+    public float maxHealth = 3f;
     private float currentHealth;
+
+    [Header("Recompensa")]
+    [Tooltip("Dinheiro que o jogador recebe")]
+    [SerializeField] private int moneyReward = 3;
+
     void Start()
     {
         currentHealth = maxHealth;
         EnemyManager.Instance?.RegisterEnemy(gameObject);
     }
+
     public void TakeDamage(float damage)
     {
-        currentHealth -= (int)damage;
+        currentHealth -= damage;
 
         if (currentHealth <= 0)
         {
             Die();
         }
     }
+
     public void Die()
     {
-        PlayerMoney.Instance.AddMoney(3);
+        PlayerMoney.Instance.AddMoney(moneyReward);
         EnemyManager.Instance?.UnregisterEnemy(gameObject);
         SoundEffectorController.PlaySoundEffect(SoundsEffects.Die);
-        Destroy(gameObject); 
+        Destroy(gameObject);
     }
 }
